41d0502e4125d09008925636c4311740899f3364
Wellington Andrade <wellington021.andrade@gmail.com>
2023-10-31 16:51:54 -0300

    json em metodo Map

--- sync_async/bin/sync_async.dart
+++ sync_async/bin/sync_async.dart
@@ -1,15 +1,38 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 Future main() async {
-  final json = await fetch();
-  print(json['id']);
+  final todo = await fetch();
+  print(todo.completed);
+  print(todo.userId);
 }
 
-Future<Map> fetch() async {
+Future<Todo> fetch() async {
   var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
   var response = await http.get(url);
   var json = jsonDecode(response.body);
-  return json;
+  var todo = Todo.fromJason(json);
+  return todo;
+}
+
+class Todo {
+  final String title;
+  final int id;
+  final int userId;
+  final bool completed;
+
+  Todo(
+      {required this.title,
+      required this.id,
+      required this.userId,
+      required this.completed});
+
+  factory Todo.fromJason(Map json) {
+    return Todo(
+      title: json['title'],
+      id: json['id'],
+      userId: json['userId'],
+      completed: json['completed']
+    );
+  }
 }
